88daee8d4d32c801ee622d29931c495e49445aaf
Indra1012 <mahindra10122002@gmail.com>
2026-02-28 23:31:24 +0530

    test deploy

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,17 +1,15 @@
 const cloudinary = require("cloudinary").v2;
 
-// configure cloudinary with env variables
+console.log("ENV CHECK →", {
+  CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  API_KEY: process.env.CLOUDINARY_API_KEY ? "Exists" : "Missing",
+  API_SECRET: process.env.CLOUDINARY_API_SECRET ? "Exists" : "Missing",
+});
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-// optional debug
-console.log("Cloudinary ENV:", {
-  name: process.env.CLOUDINARY_CLOUD_NAME,
-  key: process.env.CLOUDINARY_API_KEY,
-  secret: process.env.CLOUDINARY_API_SECRET ? "Loaded" : "Missing",
 });
 
 module.exports = cloudinary;
